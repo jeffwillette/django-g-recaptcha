c62b6ffaea6ecc1a6b132f940035d8bba836ab2d
Jeff Willette <jrwillette88@gmail.com>
2016-09-08 17:17:45 +0900

    updated requirements, removed requires file in g_recaptcha app
    Changes to requirements, added >= to prevent unwanted updates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-g-recaptcha',
-      version='0.1.10',
+      version='0.2.2',
       description='Django view decorator to validate google recaptcha forms',
       url='https://github.com/deltaskelta/django-g-recaptcha',
       author='Jeff Willette',
@@ -9,12 +9,6 @@ setup(name='django-g-recaptcha',
       keywords = ['django', 'recaptcha', 'catpcha', 'google'],
       packages = ['g_recaptcha',],
       include_package_data = True,
-      install_requires = [
-      	"Django>=1.8",
-      	"funcsigs==1.0.2",
-      	"mock==2.0.0",
-      	"pbr==1.10.0",
-      	"six==1.10.0",]
 )
 
 # The command to upload: python setup.py sdist upload -r pypi
